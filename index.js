034f1182d55ca7dfdb3059b0bed1e32af05b9f0e
okimGlitch <mhicoreynoso12@gmail.com>
2024-11-19 09:56:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use(logger)
 app.use(cors())
 //connection to mysql
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "employee",
+    host: "be1ekntamejhebinvqau-mysql.services.clever-cloud.com",
+    user: "urk1a1cxqqrqnur0",
+    password: "wFXPMK7dzdY2NFaCgIDL",
+    database: "be1ekntamejhebinvqau",
 });
 
 //initilization of connection
